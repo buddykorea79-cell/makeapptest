8fcab2e74d5b4e93f6e3bf7471bb8ffcefe42ece
buddybuddy <buddykorea79@gmail.com>
2025-10-01 17:32:15 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// config.js.example
-const CONFIG = {
-    SUPABASE_URL: 'https://htisffogpmdcnfmkpznf.supabase.co',
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0aXNmZm9ncG1kY25mbWtwem5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyMTQzNTIsImV4cCI6MjA3NDc5MDM1Mn0.WQ51_JZh2JL5AGf9qcahpLjOBE68PdmSJgEdgIBW7Wc'
-};
+const SUPABASE_URL = "https://htisffogpmdcnfmkpznf.supabase.co"; 
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh0aXNmZm9ncG1kY25mbWtwem5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyMTQzNTIsImV4cCI6MjA3NDc5MDM1Mn0.WQ51_JZh2JL5AGf9qcahpLjOBE68PdmSJgEdgIBW7Wc";
+
+// 모듈로 내보내어 app.js에서 사용할 수 있게 합니다.
+export { SUPABASE_URL, SUPABASE_ANON_KEY };
